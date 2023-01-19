8bda66c664084a6b0d32125ef8f9aaf484ee614a
Maribel <maribel.torres.guevara@gmail.com>
2023-01-19 02:43:47 -0500

    usando angular material

---
+++ src/app/material.module.ts
@@ -0,0 +1,41 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatButtonModule } from '@angular/material/button';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card';
+import { MatMenuModule } from '@angular/material/menu';
+import { LayoutModule } from '@angular/cdk/layout';
+import { NavigationComponent } from './navigation/navigation.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+
+@NgModule({
+  declarations: [DashboardComponent, NavigationComponent],
+  imports: [
+    CommonModule,
+    MatGridListModule,
+    MatCardModule,
+    MatMenuModule,
+    MatIconModule,
+    MatButtonModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatListModule,
+  ],
+  exports: [
+    MatIconModule,
+    MatDividerModule,
+    MatButtonModule,
+    MatProgressBarModule,
+    DashboardComponent,
+    NavigationComponent,
+  ],
+})
+export class MaterialModule {}
